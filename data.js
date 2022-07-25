658e73e964b1598921c5832889486544024f251b
Pedro Henrique Marques <106832134+PedroMarquesN@users.noreply.github.com>
2022-07-25 09:24:17 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,66 @@
+const data = [
+    {
+        id: 1,
+        img: "./assets/gabrielle-henderson-1DMNn6gBbwQ-unsplash",
+        nameItem: "Gabrielle Henderson",
+        value: 100,
+
+      },
+      {
+        id: 2,
+        img: "./assets/devin-avery-bMH8ub9z1_U-unsplash",
+        nameItem: "Gabrielle Henderson",
+        value: 100,
+
+      },
+      {
+        id: 3,
+        img: "./assets/devin-avery-BRVqq2uak4E-unsplash",
+        nameItem: "Gabrielle Henderson",
+        value: 100,
+
+      },
+      {
+        id: 4,
+        img: "./assets/nery-montenegro-SmEty_TVr80-unsplash",
+        nameItem: "Gabrielle Henderson",
+        value: 100,
+
+      },
+      {
+        id: 5,
+        img: "./assets/olena-sergienko-GOVTETevRm8-unsplash",
+        nameItem: "Gabrielle Henderson",
+        value: 100,
+
+      },
+      {
+        id: 6,
+        img: "./assets/pharma-hemp-complex-tAzdKblAinw-unsplash",
+        nameItem: "Gabrielle Henderson",
+        value: 100,
+
+      },
+      {
+        id: 7,
+        img: "./assets/phaedra-botanicals-uFAHrKYkTVI-unsplash",
+        nameItem: "Gabrielle Henderson",
+        value: 100,
+
+      },
+      {
+        id: 8,
+        img: "./assets/pharma-hemp-complex-yVAXSK6zFIM-unsplash",
+        nameItem: "Gabrielle Henderson",
+        value: 100,
+
+      },
+      {
+        id: 9,
+        img: "./assets/chandra-oh-y5hRv6UNKMg-unsplash",
+        nameItem: "Gabrielle Henderson",
+        value: 100,
+
+      },
+    
+]
